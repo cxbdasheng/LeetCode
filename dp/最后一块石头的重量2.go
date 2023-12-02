fb973e802c693494df15c8d7d5df03697d25b8e5
cxbdasheng <cxb163mail@163.com>
2023-12-02 15:45:05 +0800

    最后一块石头的重量2

--- dp/最后一块石头的重量2.go
+++ dp/最后一块石头的重量2.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var nums = []int{2, 7, 4, 1, 8, 1}
+	fmt.Println(lastStoneWeightII(nums))
+}
+
+func lastStoneWeightII(stones []int) int {
+	var sum int
+	for i := 0; i < len(stones); i++ {
+		sum += stones[i]
+	}
+	target := sum / 2
+	dp := make([]int, target+1)
+	for i := 0; i < len(stones); i++ {
+		for j := target; j >= stones[i]; j-- {
+			if dp[j] < dp[j-stones[i]]+stones[i] {
+				dp[j] = dp[j-stones[i]] + stones[i]
+			}
+		}
+	}
+	return sum - dp[target] - dp[target]
 }
