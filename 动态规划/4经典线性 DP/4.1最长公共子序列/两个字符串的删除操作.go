83f1103d78e1fc104f9791f8d33ff918d4bd0fb1
cxbdasheng <cxb163mail@163.com>
2024-11-23 18:42:06 +0800

    编辑距离+单词删除

--- 动态规划/4经典线性 DP/4.1最长公共子序列/两个字符串的删除操作.go
+++ 动态规划/4经典线性 DP/4.1最长公共子序列/两个字符串的删除操作.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
+// 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+// 每步 可以删除任意一个字符串中的一个字符。
 func main() {
 	fmt.Println(minDistance("sea", "eat"))
+	fmt.Println(minDistance1("sea", "eat"))
 }
 
+// 输入: word1 = "sea", word2 = "eat"
+// 输出: 2
+// 解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+
+// 输入：word1 = "leetcode", word2 = "etco"
+// 输出：4
+
+/*
+*
+减去最长公共子序列
+*/
 func minDistance(word1 string, word2 string) int {
 	n := len(word1)
 	m := len(word2)
@@ -22,5 +36,33 @@ func minDistance(word1 string, word2 string) int {
 			}
 		}
 	}
-	return dp[n][m]
+	return n + m - dp[n][m]*2
+}
+
+/*
+*
+动态规划直接做
+*/
+func minDistance1(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+	}
+	//初始化
+	for i := 0; i < len(dp); i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j < len(dp[0]); j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[i]); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+2)
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
 }
