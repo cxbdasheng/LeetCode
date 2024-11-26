79e57aed808f43e3ffca52feb83805cde73d5bd0
cxbdasheng <cxb163mail@163.com>
2024-11-26 14:49:20 +0800

    两个字符串的最小ASCII删除和

--- 动态规划/4经典线性 DP/4.1最长公共子序列/两个字符串的最小ASCII删除和.go
+++ 动态规划/4经典线性 DP/4.1最长公共子序列/两个字符串的最小ASCII删除和.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+// 给定两个字符串s1 和 s2，返回 使两个字符串相等所需删除字符的 ASCII 值的最小和 。
 func main() {
 	fmt.Println(minimumDeleteSum("delete", "leet"))
 }
+
+// 输入: s1 = "sea", s2 = "eat"
+// 输出: 231
+// 解释: 在 "sea" 中删除 "s" 并将 "s" 的值(115)加入总和。
+// 在 "eat" 中删除 "t" 并将 116 加入总和。
+// 结束时，两个字符串相等，115 + 116 = 231 就是符合条件的最小和。
+
+// 输入: s1 = "delete", s2 = "leet"
+// 输出: 403
+// 解释: 在 "delete" 中删除 "dee" 字符串变成 "let"，
+// 将 100[d]+101[e]+101[e] 加入总和。在 "leet" 中删除 "e" 将 101[e] 加入总和。
+// 结束时，两个字符串都等于 "let"，结果即为 100+101+101+101 = 403 。
+// 如果改为将两个字符串转换为 "lee" 或 "eet"，我们会得到 433 或 417 的结果，比答案更大。
 func minimumDeleteSum(s1 string, s2 string) int {
 	n1 := len(s1)
 	n2 := len(s2)
